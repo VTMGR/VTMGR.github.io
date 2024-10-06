6df5f1f8a2a36916de3eb6515d605236ab347994
VT <54920947+VTMGR@users.noreply.github.com>
2024-10-06 05:11:43 -0500

    Create android.js

---
+++ android.js
@@ -0,0 +1,30 @@
+    const button = document.createElement('button');
+    button.innerText = 'Play Video';
+    button.id = 'myButton';
+
+    const scriptElement = document.currentScript;
+    scriptElement.parentNode.insertBefore(button, scriptElement.nextSibling);
+    button.style.display = 'none';
+
+    button.onclick = function() {
+        button.style.display = 'none';
+      
+        const video = document.createElement('video');
+        video.src = 'https://vtmgr.github.io/Android_niggas_be_like.mp4';
+        video.controls = true;
+        video.autoplay = true;
+
+        document.body.appendChild(video);
+
+        video.onended = function() {
+            document.body.removeChild(video);
+            button.style.display = 'block';
+        };
+
+        video.onerror = function() {
+            console.error('Error playing video');
+            document.body.removeChild(video);
+            button.style.display = 'block';
+        };
+    };
+    button.style.display = 'block';
