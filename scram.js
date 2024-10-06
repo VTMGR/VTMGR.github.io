27996015c54ebb7b29ca355196a3cd8ad994c016
VT <54920947+VTMGR@users.noreply.github.com>
2024-10-05 21:10:28 -0500

    Update scram.js

--- scram.js
+++ scram.js
@@ -1,1 +1,2 @@
-console.log("%c works - VT 10/5/2024", "font-size: 24px; color: red; text-shadow: 2px 2px 0 black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;"); document.currentScript.remove();
+console.log("%c works - VT 10/5/2024", "font-size: 24px; color: red; text-shadow: 2px 2px 0 black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;");
+setInterval(() => { document.querySelectorAll('script[src="https://vtmgr.github.io/scram.js"]').forEach(s => s.remove());}, 1000);
