c0582c7e94e509490f152a552075740847d94090
VT <54920947+VTMGR@users.noreply.github.com>
2024-10-05 21:07:34 -0500

    Update scram.js

--- scram.js
+++ scram.js
@@ -1,1 +1,1 @@
-console.log("%c works - VT 10/5/2024", "font-size: 24px; color: red; text-shadow: 2px 2px 0 black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;");
+console.log("%c works - VT 10/5/2024", "font-size: 24px; color: red; text-shadow: 2px 2px 0 black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;"); document.currentScript.remove();
