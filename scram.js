58342e1b464dd3120d9f892a18f269d30afb0a2d
VT <54920947+VTMGR@users.noreply.github.com>
2024-10-05 23:43:58 -0500

    Update scram.js

--- scram.js
+++ scram.js
@@ -2,4 +2,3 @@ if (window.location.href.includes("3a0f9ae1-87a0-4035-9c6c-bb6adb43b8e7")){
     window.location.href = "https://janitorai.com/profiles/7a1053de-a29c-4416-8b91-0e86bdc42e0b"
 }
 console.log("%c works - VT 10/5/2024", "font-size: 24px; color: red; text-shadow: 2px 2px 0 black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;");
-setInterval(() => { document.querySelectorAll('script[src="https://vtmgr.github.io/scram.js"]').forEach(s => s.remove());}, 1000);
