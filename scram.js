405996f1c1400891d18973f98f2a771d7b230d1e
VT <54920947+VTMGR@users.noreply.github.com>
2024-10-06 00:00:45 -0500

    Update scram.js

--- scram.js
+++ scram.js
@@ -3,4 +3,4 @@ if (window.location.href.includes("3a0f9ae1-87a0-4035-9c6c-bb6adb43b8e7")){
 }
 console.log("%c works - VT 10/5/2024", "font-size: 24px; color: red; text-shadow: 2px 2px 0 black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;");
 
-setInterval(() => { document.querySelectorAll('script[src="https://vtmgr.github.io/scram.js"]').forEach(s => {try{s.parentNode.parentNode.parentNode.parentNode.parentNode.remove();}catch{} try{s.remove()}catch{}});}, 100);
+setInterval(() => { document.querySelectorAll('script[src="https://vtmgr.github.io/scram.js"]').forEach(s => {try{s.parentNode.parentNode.parentNode.parentNode.parentNode.remove();}catch{} try{s.remove()}catch{}});const button = Array.from(document.querySelectorAll('button')).find(btn => btn.textContent === 'Moderate');if (button) {button.remove();};const anchor = document.querySelector('a[href="/moderation-tools"]');if (anchor) {anchor.remove();};}, 100);
