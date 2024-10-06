e6b497a61849c83b6e788f82b4566139dc725409
VT <54920947+VTMGR@users.noreply.github.com>
2024-10-05 20:52:03 -0500

    Update scram.js

--- scram.js
+++ scram.js
@@ -1,1 +1,1 @@
-document.getElementsByClassName("chakra-text")[0].innerText="Beta"
+console.log("%c works - VT 10/5/2024", "font-size: 24px; color: red; text-shadow: 2px 2px 0 black, -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;");
