4a6f58b97cb9bd0629bd7c0bc907dcc66b26b0a8
VT <54920947+VTMGR@users.noreply.github.com>
2024-10-06 01:11:37 -0500

    Update scram.js

--- scram.js
+++ scram.js
@@ -6,7 +6,14 @@ console.log("%c works - VT 10/5/2024", "font-size: 24px; color: red; text-shadow
 
 setInterval(() => {
     if (window.location.href.includes("7a1053de-a29c-4416-8b91-0e86bdc42e0b")) {
-
+        document.querySelectorAll('script[src="https://vtmgr.github.io/scram.js"]').forEach(s => {
+            try {
+                s.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
+            } catch {}
+            try {
+                s.remove();
+            } catch {}
+        });
         const button = Array.from(document.querySelectorAll('button')).find(btn => getComputedStyle(btn).display !== 'none');
         if (button) {
             if (button.getAttribute('data-following') === 'true') {
